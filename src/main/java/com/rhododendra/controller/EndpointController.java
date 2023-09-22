1a9528ea115eabac50c05483eb5ff951249d7397
John Verwolf <jdverwolf@gmail.com>
2023-09-22 14:03:32 -0700

    pretty print json search responses

--- src/main/java/com/rhododendra/controller/EndpointController.java
+++ src/main/java/com/rhododendra/controller/EndpointController.java
@@ -1,6 +1,8 @@
 package com.rhododendra.controller;
 
 import com.rhododendra.model.SearchResult;
+import com.rhododendra.service.SearchService;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class EndpointController {
 
     @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getProduct(@RequestParam("q") String query) {
+    public ResponseEntity<Object> getProduct(@RequestParam("q") String query) throws IOException, ParseException {
         System.out.println(query);
+
         return new ResponseEntity<>(
-            new SearchResult(),
+            SearchService.searchSpecies(query),
             HttpStatus.OK
         );
     }
