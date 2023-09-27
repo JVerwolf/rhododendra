1e7b5041373846e2386e138b093566b838502d34
John Verwolf <jdverwolf@gmail.com>
2023-09-27 08:39:10 -0700

    Add image controller

--- src/main/java/com/rhododendra/controller/EndpointController.java
+++ src/main/java/com/rhododendra/controller/EndpointController.java
@@ -5,9 +5,7 @@ import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -15,23 +13,19 @@ import java.io.IOException;
 public class EndpointController {
 
     @GetMapping(value = "/search_json", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> getProduct(@RequestParam("q") String query) throws IOException, ParseException {
+    public ResponseEntity<Object> searchJson(@RequestParam("q") String query) throws IOException, ParseException {
         System.out.println(query);
 
         return new ResponseEntity<>(
-            SearchService.searchBotanists(query),
+            SearchService.searchSpecies(query),
             HttpStatus.OK
         );
     }
 
-
-//    @GetMapping(value = "/search_json", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<Object> getProduct(@RequestParam("q") String query) throws IOException, ParseException {
-//        System.out.println(query);
-//
-//        return new ResponseEntity<>(
-//            SearchService.searchSpecies(query),
-//            HttpStatus.OK
-//        );
+//    @RequestMapping(value = "/pictures/normal/{id:.+}", method = RequestMethod.GET)
+//    public ResponseEntity<byte[]> getImage(@PathVariable("id") String id) {
+//        System.out.println(id);
+////        byte[] image = imageService.getImage(id);
+//        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
 //    }
 }
---
+++ src/main/java/com/rhododendra/controller/ImageController.java
@@ -0,0 +1,15 @@
+package com.rhododendra.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ImageController implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+            .addResourceHandler("/pictures/**")
+            .addResourceLocations("file:/Users/john.verwolf/code/hirsutum_save/websites/www.hirsutum.info/rhododendron/species/pictures/");
+    }
+}
